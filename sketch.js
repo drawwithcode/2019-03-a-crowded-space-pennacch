e25c94157b1a26f4825ab94907794bbc3997c31b
pennacch <55466516+pennacch@users.noreply.github.com>
2019-10-08 17:32:56 +0200

    x

--- sketch.js
+++ sketch.js
@@ -1,11 +1,39 @@
-function preload(){
+var anotherRectangle;
+
+function preload() {
   // put preload code here
 }
 
 function setup() {
   // put setup code here
+  createCanvas(windowWidth, windowHeight);
 }
 
+
 function draw() {
-  // put drawing code here
+
+  for (var i = 0; i < 10; i++) {
+    anotherRectangle = new Rectangle(windowWidth / 6 - 50 + random() * windowWidth / 3 * 2, windowHeight / 6 - 70 + random() * windowHeight / 3 * 2, 50, 70);
+    anotherRectangle.display();
+  }
+}
+
+function Rectangle(_x, _y, _w, _h) {
+
+  this.width = _w;
+  this.height = _h;
+  this.x = _x;
+  this.y = _y;
+
+  this.display = function() {
+    strokeWeight(0.5);
+    noFill();
+    rect(this.x, this.y, this.width, this.height);
+  }
+  this.move = function() {
+    this.x += random() * 2;
+    this.y += random();
+
+  }
+
 }
